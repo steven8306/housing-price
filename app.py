5acbefda1f14aa8a88511f4a3af37be1e422ae30
steven8306 <stevenjohn201315@gmail.com>
2025-03-09 09:49:40 -0700

    priliminary

--- app.py
+++ app.py
@@ -1,1 +1,186 @@
-git 
+import os
+import warnings
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from IPython.display import display
+from pandas.api.types import CategoricalDtype
+
+from category_encoders import MEstimateEncoder
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import mutual_info_regression
+from sklearn.model_selection import KFold, cross_val_score
+from xgboost import XGBRegressor
+
+
+# Set Matplotlib defaults
+plt.style.use("seaborn-v0_8-white")
+plt.rc("figure", autolayout=True)
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=14,
+    titlepad=10,
+)
+
+# Mute warnings
+warnings.filterwarnings('ignore')
+
+#data cleaning
+def clean(df):
+    df["Exterior2nd"] = df["Exterior2nd"].replace({"Brk Cmn": "BrkComm"})
+    # Some values of GarageYrBlt are corrupt, so we'll replace them
+    # with the year the house was built
+    df["GarageYrBlt"] = df["GarageYrBlt"].where(df.GarageYrBlt <= 2010, df.YearBuilt)
+    # Names beginning with numbers are awkward to work with
+    df.rename(columns={
+        "1stFlrSF": "FirstFlrSF",
+        "2ndFlrSF": "SecondFlrSF",
+        "3SsnPorch": "Threeseasonporch",
+    }, inplace=True,
+    )
+    return df
+
+#Encode statistical data
+# The nominative (unordered) categorical features
+features_nom = ["MSSubClass", "MSZoning", "Street", "Alley", "LandContour", "LotConfig", "Neighborhood", "Condition1", "Condition2", "BldgType", "HouseStyle", "RoofStyle", "RoofMatl", "Exterior1st", "Exterior2nd", "MasVnrType", "Foundation", "Heating", "CentralAir", "GarageType", "MiscFeature", "SaleType", "SaleCondition"]
+
+
+# The ordinal (ordered) categorical features 
+
+# Pandas calls the categories "levels"
+five_levels = ["Po", "Fa", "TA", "Gd", "Ex"]
+ten_levels = list(range(10))
+
+ordered_levels = {
+    "OverallQual": ten_levels,
+    "OverallCond": ten_levels,
+    "ExterQual": five_levels,
+    "ExterCond": five_levels,
+    "BsmtQual": five_levels,
+    "BsmtCond": five_levels,
+    "HeatingQC": five_levels,
+    "KitchenQual": five_levels,
+    "FireplaceQu": five_levels,
+    "GarageQual": five_levels,
+    "GarageCond": five_levels,
+    "PoolQC": five_levels,
+    "LotShape": ["Reg", "IR1", "IR2", "IR3"],
+    "LandSlope": ["Sev", "Mod", "Gtl"],
+    "BsmtExposure": ["No", "Mn", "Av", "Gd"],
+    "BsmtFinType1": ["Unf", "LwQ", "Rec", "BLQ", "ALQ", "GLQ"],
+    "BsmtFinType2": ["Unf", "LwQ", "Rec", "BLQ", "ALQ", "GLQ"],
+    "Functional": ["Sal", "Sev", "Maj1", "Maj2", "Mod", "Min2", "Min1", "Typ"],
+    "GarageFinish": ["Unf", "RFn", "Fin"],
+    "PavedDrive": ["N", "P", "Y"],
+    "Utilities": ["NoSeWa", "NoSewr", "AllPub"],
+    "CentralAir": ["N", "Y"],
+    "Electrical": ["Mix", "FuseP", "FuseF", "FuseA", "SBrkr"],
+    "Fence": ["MnWw", "GdWo", "MnPrv", "GdPrv"],
+}
+
+# Add a None level for missing values
+ordered_levels = {key: ["None"] + value for key, value in
+                  ordered_levels.items()}
+
+#encode data
+def encode(df):
+    # Nominal categories
+    for name in features_nom:
+        df[name] = df[name].astype("category")
+        # Add a None category for missing values
+        if "None" not in df[name].cat.categories:
+            df[name] = df[name].cat.add_categories("None")
+    # Ordinal categories
+    for name, levels in ordered_levels.items():
+        df[name] = df[name].astype(CategoricalDtype(levels,
+                                                    ordered=True))
+    return df
+
+#impute data
+def impute(df):
+    for name in df.select_dtypes("number"):
+        df[name] = df[name].fillna(0)
+    for name in df.select_dtypes("category"):
+        df[name] = df[name].fillna("None")
+    return df
+
+
+def load_data():
+    # Read data
+    df_train = pd.read_csv("./train.csv", index_col="Id")
+    df_test = pd.read_csv("./test.csv", index_col="Id")
+    # Merge the splits so we can process them together
+    df = pd.concat([df_train, df_test])
+    # Preprocessing
+    df = clean(df)
+    df = encode(df)
+    df = impute(df)
+    # Reform splits
+    df_train = df.loc[df_train.index, :]
+    df_test = df.loc[df_test.index, :]
+    return df_train, df_test
+
+df_train, df_test = load_data()
+# display(df_train)
+# display(df_test)
+
+def score_dataset(X, y, model=XGBRegressor()):
+    # Label encoding for categoricals
+    #
+    # Label encoding is good for XGBoost and RandomForest, but one-hot
+    # would be better for models like Lasso or Ridge. The `cat.codes`
+    # attribute holds the category levels.
+    for colname in X.select_dtypes(["category"]):
+        X[colname] = X[colname].cat.codes
+    # Metric for Housing competition is RMSLE (Root Mean Squared Log Error)
+    log_y = np.log(y)
+    score = cross_val_score(
+        model, X, log_y, cv=5, scoring="neg_mean_squared_error",
+    )
+    score = -1 * score.mean()
+    score = np.sqrt(score)
+    return score
+
+X = df_train.copy()
+y = X.pop("SalePrice")
+
+def make_mi_scores(X, y):
+    X = X.copy()
+    for colname in X.select_dtypes(["object", "category"]):
+        X[colname], _ = X[colname].factorize()
+    # All discrete features should now have integer dtypes
+    discrete_features = [pd.api.types.is_integer_dtype(t) for t in X.dtypes]
+    mi_scores = mutual_info_regression(X, y, discrete_features=discrete_features, random_state=0)
+    mi_scores = pd.Series(mi_scores, name="MI Scores", index=X.columns)
+    mi_scores = mi_scores.sort_values(ascending=False)
+    return mi_scores
+
+
+def plot_mi_scores(scores):
+    scores = scores.sort_values(ascending=True)
+    width = np.arange(len(scores))
+    ticks = list(scores.index)
+    plt.barh(width, scores)
+    plt.yticks(width, ticks)
+    plt.title("Mutual Information Scores")
+
+baseline_score = score_dataset(X, y)
+print(f"Baseline score: {baseline_score:.5f} RMSLE")
+
+mi_scores = make_mi_scores(X, y)
+print(mi_scores)
+
+def drop_uninformative(df, mi_scores):
+    return df.loc[:, mi_scores > 0.0]
+
+X = drop_uninformative(X, mi_scores)
+print(score_dataset(X, y))
+
+#create features
