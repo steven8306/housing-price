a64813db8591a3908918c81c0e17819b713e58b4
steven8306 <stevenjohn201315@gmail.com>
2025-03-10 00:51:50 -0700

    final

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ baseline_score = score_dataset(X, y)
 print(f"Baseline score: {baseline_score:.5f} RMSLE")
 
 mi_scores = make_mi_scores(X, y)
-print(mi_scores)
+# print(mi_scores)
 
 def drop_uninformative(df, mi_scores):
     return df.loc[:, mi_scores > 0.0]
@@ -184,3 +184,301 @@ X = drop_uninformative(X, mi_scores)
 print(score_dataset(X, y))
 
 #create features
+
+def mathematical_transforms(df):
+    X = pd.DataFrame()  # dataframe to hold new features
+    X["LivLotRatio"] = df.GrLivArea / df.LotArea
+    X["Spaciousness"] = (df.FirstFlrSF + df.SecondFlrSF) / df.TotRmsAbvGrd
+    # This feature ended up not helping performance
+    # X["TotalOutsideSF"] = \
+    #     df.WoodDeckSF + df.OpenPorchSF + df.EnclosedPorch + \
+    #     df.Threeseasonporch + df.ScreenPorch
+    return X
+
+
+def interactions(df):
+    X = pd.get_dummies(df.BldgType, prefix="Bldg")
+    X = X.mul(df.GrLivArea, axis=0)
+    return X
+
+
+def counts(df):
+    X = pd.DataFrame()
+    X["PorchTypes"] = df[[
+        "WoodDeckSF",
+        "OpenPorchSF",
+        "EnclosedPorch",
+        "Threeseasonporch",
+        "ScreenPorch",
+    ]].gt(0.0).sum(axis=1)
+    return X
+
+
+def break_down(df):
+    X = pd.DataFrame()
+    X["MSClass"] = df.MSSubClass.str.split("_", n=1, expand=True)[0]
+    return X
+
+
+def group_transforms(df):
+    X = pd.DataFrame()
+    X["MedNhbdArea"] = df.groupby("Neighborhood")["GrLivArea"].transform("median")
+    return X
+cluster_features = [
+    "LotArea",
+    "TotalBsmtSF",
+    "FirstFlrSF",
+    "SecondFlrSF",
+    "GrLivArea",
+]
+
+
+def cluster_labels(df, features, n_clusters=20):
+    X = df.copy()
+    X_scaled = X.loc[:, features]
+    X_scaled = (X_scaled - X_scaled.mean(axis=0)) / X_scaled.std(axis=0)
+    kmeans = KMeans(n_clusters=n_clusters, n_init=50, random_state=0)
+    X_new = pd.DataFrame()
+    X_new["Cluster"] = kmeans.fit_predict(X_scaled)
+    return X_new
+
+
+def cluster_distance(df, features, n_clusters=20):
+    X = df.copy()
+    X_scaled = X.loc[:, features]
+    X_scaled = (X_scaled - X_scaled.mean(axis=0)) / X_scaled.std(axis=0)
+    kmeans = KMeans(n_clusters=20, n_init=50, random_state=0)
+    X_cd = kmeans.fit_transform(X_scaled)
+    # Label features and join to dataset
+    X_cd = pd.DataFrame(
+        X_cd, columns=[f"Centroid_{i}" for i in range(X_cd.shape[1])]
+    )
+    return X_cd
+
+def apply_pca(X, standardize=True):
+    # Standardize
+    if standardize:
+        X = (X - X.mean(axis=0)) / X.std(axis=0)
+    # Create principal components
+    pca = PCA()
+    X_pca = pca.fit_transform(X)
+    # Convert to dataframe
+    component_names = [f"PC{i+1}" for i in range(X_pca.shape[1])]
+    X_pca = pd.DataFrame(X_pca, columns=component_names)
+    # Create loadings
+    loadings = pd.DataFrame(
+        pca.components_.T,  # transpose the matrix of loadings
+        columns=component_names,  # so the columns are the principal components
+        index=X.columns,  # and the rows are the original features
+    )
+    return pca, X_pca, loadings
+
+
+def plot_variance(pca, width=8, dpi=100):
+    # Create figure
+    fig, axs = plt.subplots(1, 2)
+    n = pca.n_components_
+    grid = np.arange(1, n + 1)
+    # Explained variance
+    evr = pca.explained_variance_ratio_
+    axs[0].bar(grid, evr)
+    axs[0].set(
+        xlabel="Component", title="% Explained Variance", ylim=(0.0, 1.0)
+    )
+    # Cumulative Variance
+    cv = np.cumsum(evr)
+    axs[1].plot(np.r_[0, grid], np.r_[0, cv], "o-")
+    axs[1].set(
+        xlabel="Component", title="% Cumulative Variance", ylim=(0.0, 1.0)
+    )
+    # Set up figure
+    fig.set(figwidth=8, dpi=100)
+    return axs
+def pca_inspired(df):
+    X = pd.DataFrame()
+    X["Feature1"] = df.GrLivArea + df.TotalBsmtSF
+    X["Feature2"] = df.YearRemodAdd * df.TotalBsmtSF
+    return X
+
+
+def pca_components(df, features):
+    X = df.loc[:, features]
+    _, X_pca, _ = apply_pca(X)
+    return X_pca
+
+
+pca_features = [
+    "GarageArea",
+    "YearRemodAdd",
+    "TotalBsmtSF",
+    "GrLivArea",
+]
+def corrplot(df, method="pearson", annot=True, **kwargs):
+    sns.clustermap(
+        df.corr(method, numeric_only=True),
+        vmin=-1.0,
+        vmax=1.0,
+        cmap="icefire",
+        method="complete",
+        annot=annot,
+        **kwargs,
+    )
+
+def label_encode(df):
+    X = df.copy()
+    for colname in X.select_dtypes(["category"]):
+        X[colname] = X[colname].cat.codes
+    return X
+
+corrplot(df_train, annot=None)
+
+class CrossFoldEncoder:
+    def __init__(self, encoder, **kwargs):
+        self.encoder_ = encoder
+        self.kwargs_ = kwargs  # keyword arguments for the encoder
+        self.cv_ = KFold(n_splits=5)
+
+    # Fit an encoder on one split and transform the feature on the
+    # other. Iterating over the splits in all folds gives a complete
+    # transformation. We also now have one trained encoder on each
+    # fold.
+    def fit_transform(self, X, y, cols):
+        self.fitted_encoders_ = []
+        self.cols_ = cols
+        X_encoded = []
+        for idx_encode, idx_train in self.cv_.split(X):
+            fitted_encoder = self.encoder_(cols=cols, **self.kwargs_)
+            fitted_encoder.fit(
+                X.iloc[idx_encode, :], y.iloc[idx_encode],
+            )
+            X_encoded.append(fitted_encoder.transform(X.iloc[idx_train, :])[cols])
+            self.fitted_encoders_.append(fitted_encoder)
+        X_encoded = pd.concat(X_encoded)
+        X_encoded.columns = [name + "_encoded" for name in X_encoded.columns]
+        return X_encoded
+
+    # To transform the test data, average the encodings learned from
+    # each fold.
+    def transform(self, X):
+        from functools import reduce
+
+        X_encoded_list = []
+        for fitted_encoder in self.fitted_encoders_:
+            X_encoded = fitted_encoder.transform(X)
+            X_encoded_list.append(X_encoded[self.cols_])
+        X_encoded = reduce(
+            lambda x, y: x.add(y, fill_value=0), X_encoded_list
+        ) / len(X_encoded_list)
+        X_encoded.columns = [name + "_encoded" for name in X_encoded.columns]
+        return X_encoded
+
+def create_features(df, df_test=None):
+    X = df.copy()
+    y = X.pop("SalePrice")
+    mi_scores = make_mi_scores(X, y)
+
+    # Combine splits if test data is given
+    #
+    # If we're creating features for test set predictions, we should
+    # use all the data we have available. After creating our features,
+    # we'll recreate the splits.
+    if df_test is not None:
+        X_test = df_test.copy()
+        X_test.pop("SalePrice")
+        X = pd.concat([X, X_test])
+
+    # Lesson 2 - Mutual Information
+    X = drop_uninformative(X, mi_scores)
+
+    # Lesson 3 - Transformations
+    X = X.join(mathematical_transforms(X))
+    X = X.join(interactions(X))
+    X = X.join(counts(X))
+    # X = X.join(break_down(X))
+    X = X.join(group_transforms(X))
+
+    # Lesson 4 - Clustering
+    # X = X.join(cluster_labels(X, cluster_features, n_clusters=20))
+    # X = X.join(cluster_distance(X, cluster_features, n_clusters=20))
+
+    # Lesson 5 - PCA
+    X = X.join(pca_inspired(X))
+    # X = X.join(pca_components(X, pca_features))
+    # X = X.join(indicate_outliers(X))
+
+    X = label_encode(X)
+
+    # Reform splits
+    if df_test is not None:
+        X_test = X.loc[df_test.index, :]
+        X.drop(df_test.index, inplace=True)
+
+    # Lesson 6 - Target Encoder
+    encoder = CrossFoldEncoder(MEstimateEncoder, m=1)
+    X = X.join(encoder.fit_transform(X, y, cols=["MSSubClass"]))
+    if df_test is not None:
+        X_test = X_test.join(encoder.transform(X_test))
+
+    if df_test is not None:
+        return X, X_test
+    else:
+        return X
+
+
+df_train, df_test = load_data()
+X_train = create_features(df_train)
+y_train = df_train.loc[:, "SalePrice"]
+
+print(score_dataset(X_train, y_train))
+xgb_params = dict(
+    max_depth=6,           # maximum depth of each tree - try 2 to 10
+    learning_rate=0.01,    # effect of each tree - try 0.0001 to 0.1
+    n_estimators=1000,     # number of trees (that is, boosting rounds) - try 1000 to 8000
+    min_child_weight=1,    # minimum number of houses in a leaf - try 1 to 10
+    colsample_bytree=0.7,  # fraction of features (columns) per tree - try 0.2 to 1.0
+    subsample=0.7,         # fraction of instances (rows) per tree - try 0.2 to 1.0
+    reg_alpha=0.5,         # L1 regularization (like LASSO) - try 0.0 to 10.0
+    reg_lambda=1.0,        # L2 regularization (like Ridge) - try 0.0 to 10.0
+    num_parallel_tree=1,   # set > 1 for boosted random forests
+)
+
+xgb = XGBRegressor(**xgb_params)
+print(score_dataset(X_train, y_train, xgb))
+
+#optuna
+
+import optuna
+
+def objective(trial):
+    xgb_params = dict(
+        max_depth=trial.suggest_int("max_depth", 2, 10),
+        learning_rate=trial.suggest_float("learning_rate", 1e-4, 1e-1, log=True),
+        n_estimators=trial.suggest_int("n_estimators", 1000, 8000),
+        min_child_weight=trial.suggest_int("min_child_weight", 1, 10),
+        colsample_bytree=trial.suggest_float("colsample_bytree", 0.2, 1.0),
+        subsample=trial.suggest_float("subsample", 0.2, 1.0),
+        reg_alpha=trial.suggest_float("reg_alpha", 1e-4, 1e2, log=True),
+        reg_lambda=trial.suggest_float("reg_lambda", 1e-4, 1e2, log=True),
+    )
+    xgb = XGBRegressor(**xgb_params)
+    return score_dataset(X_train, y_train, xgb)
+
+study = optuna.create_study(direction="minimize")
+study.optimize(objective, n_trials=20)
+xgb_params = study.best_params
+xgb = XGBRegressor(**xgb_params)
+print(score_dataset(X_train, y_train, xgb))
+
+#model traning
+X_train, X_test = create_features(df_train, df_test)
+y_train = df_train.loc[:, "SalePrice"]
+
+xgb = XGBRegressor(**xgb_params)
+# XGB minimizes MSE, but competition loss is RMSLE
+# So, we need to log-transform y to train and exp-transform the predictions
+xgb.fit(X_train, np.log(y))
+predictions = np.exp(xgb.predict(X_test))
+
+output = pd.DataFrame({'Id': X_test.index, 'SalePrice': predictions}git )
+output.to_csv('my_submission.csv', index=False)
+print("Your submission was successfully saved!")
